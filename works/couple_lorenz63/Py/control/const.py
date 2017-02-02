3dfa4906cf5948cbf4cd645ecbe753625aefb26f
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-02-01 19:01:08 -0500

    Revert "3 variables test"
    This reverts commit c8486cec9276761b371e6884759aac9f82b8b1c0.

--- works/couple_lorenz63/Py/control/const.py
+++ works/couple_lorenz63/Py/control/const.py
@@ -3,7 +3,7 @@
 import math
 import numpy as np
 
-DIMM = 3    # dimension of model variable n
+DIMM = 9    # dimension of model variable n
 DIMO = DIMM # dimension of observation variable m
 
 DT = 0.01
@@ -18,7 +18,7 @@ FERR_INI = 10.0
 
 # name, inf, aint, diag(h), nmem, method
 EXPLIST = [ \
-  {"name":"xyz_long",  "inf":1.20, "aint":25, "diag":[1.0,1.0,1.0], "nmem":3, "method":"etkf"}, \
+  {"name":"xyz_long",  "inf":1.20, "aint":25, "diag":[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], "nmem":3, "method":"etkf"}, \
   # {"name":"xyz_short", "inf":1.05, "aint":8,  "diag":[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], "nmem":3, "method":"etkf"}, \
   # {"name":"xy",        "inf":1.05, "aint":8,  "diag":[1.0,1.0,0.0,1.0,1.0,0.0,1.0,1.0,0.0], "nmem":3, "method":"etkf"}, \
   # {"name":"yz",        "inf":1.05, "aint":8,  "diag":[0.0,1.0,1.0,0.0,1.0,1.0,0.0,1.0,1.0], "nmem":3, "method":"etkf"}, \
@@ -27,7 +27,7 @@ EXPLIST = [ \
   # {"name":"y",         "inf":1.05, "aint":8,  "diag":[0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,0.0], "nmem":3, "method":"etkf"}, \
   # {"name":"z",         "inf":1.05, "aint":8,  "diag":[0.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0], "nmem":3, "method":"etkf"}, \
   # {"name":"xyz_3dvar", "inf":1.0 , "aint":8,  "diag":[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], "nmem":1, "method":"3dvar"},\
-  {"name":"xyz_4dvar", "inf":1.0 , "aint":25, "diag":[1.0,1.0,1.0], "nmem":1, "method":"4dvar"} \
+  {"name":"xyz_4dvar", "inf":1.0 , "aint":25, "diag":[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], "nmem":1, "method":"4dvar"} \
 ]
 
 def getr():
--- works/couple_lorenz63/Py/model/model.py
+++ works/couple_lorenz63/Py/model/model.py
@@ -21,43 +21,33 @@ def timestep(x):
 # input:  np.array(DIMM)
 # output: np.array(DIMM)
 def tendency(x):
-  # uncoupled
+  ### model constants
+  # dynamic
   sigma = 10.0
-  r = 28.0
-  b = 8.0 / 3.0
-  k = np.empty((3))
-  k[0] = -sigma * x[0]            + sigma * x[1]
-  k[1] =  -x[0] * x[2] + r * x[0] -         x[1]
-  k[2] =   x[0] * x[1] - b * x[2]
-  return k
+  r     = 28.0
+  b     = 8.0 / 3.0
+  # coupling
+  tau   = 0.1
+  c     = 1.0
+  cz    = 1.0
+  ce    = 0.08
+  # offset and scaling
+  s     = 1.0
+  k1    = 10.0
+  k2    = -11.0
 
-  # ### model constants
-  # # dynamic
-  # sigma = 10.0
-  # r     = 28.0
-  # b     = 8.0 / 3.0
-  # # coupling
-  # tau   = 0.1
-  # c     = 1.0
-  # cz    = 1.0
-  # ce    = 0.08
-  # # offset and scaling
-  # s     = 1.0
-  # k1    = 10.0
-  # k2    = -11.0
-
-  # dx = np.empty((DIMM))
-  # # extratropic atm
-  # dx[0] =           -sigma * x[0] + sigma * x[1] - ce * (s * x[3] + k1)
-  # dx[1] = -x[0] * x[2] + r * x[0] -         x[1] + ce * (s * x[4] + k1)
-  # dx[2] =  x[0] * x[1] - b * x[2]
-  # # tropic atm
-  # dx[3] =           -sigma * x[3] + sigma * x[4] - c  * (s * x[6] + k2) - ce * (s * x[0] + k1)
-  # dx[4] = -x[3] * x[5] + r * x[3] -         x[4] + c  * (s * x[7] + k2) + ce * (s * x[1] + k1)
-  # dx[5] =  x[3] * x[4] - b * x[5]                + cz * x[8]
-  # # tropic ocn
-  # dx[6] = tau * (              -sigma * x[6] + sigma * x[7]) - c  * (x[3] + k2)
-  # dx[7] = tau * (-s * x[6] * x[8] + r * x[6] -         x[7]) + c  * (x[4] + k2)
-  # dx[8] = tau * ( s * x[6] * x[7] - b * x[8]               ) - cz * x[5]
-  # return dx
+  dx = np.empty((DIMM))
+  # extratropic atm
+  dx[0] =           -sigma * x[0] + sigma * x[1] - ce * (s * x[3] + k1)
+  dx[1] = -x[0] * x[2] + r * x[0] -         x[1] + ce * (s * x[4] + k1)
+  dx[2] =  x[0] * x[1] - b * x[2]
+  # tropic atm
+  dx[3] =           -sigma * x[3] + sigma * x[4] - c  * (s * x[6] + k2) - ce * (s * x[0] + k1)
+  dx[4] = -x[3] * x[5] + r * x[3] -         x[4] + c  * (s * x[7] + k2) + ce * (s * x[1] + k1)
+  dx[5] =  x[3] * x[4] - b * x[5]                + cz * x[8]
+  # tropic ocn
+  dx[6] = tau * (              -sigma * x[6] + sigma * x[7]) - c  * (x[3] + k2)
+  dx[7] = tau * (-s * x[6] * x[8] + r * x[6] -         x[7]) + c  * (x[4] + k2)
+  dx[8] = tau * ( s * x[6] * x[7] - b * x[8]               ) - cz * x[5]
+  return dx
 
