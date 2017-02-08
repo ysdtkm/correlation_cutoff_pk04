51d85f6d3eb7953fc74ca97f4c1916e26251dd07
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-02-08 14:32:30 -0500

    add tentative tanjent model

--- works/couple_lorenz63/Py/model.py
+++ works/couple_lorenz63/Py/model.py
@@ -62,25 +62,85 @@ def tendency(x):
     dx[8] = tau * ( s * x[6] * x[7] - b * x[8]               ) - cz * x[5]
     return dx
 
-# x      <- np.array(DIMM)
-# return -> np.array(DIMM,DIMM)
+### NEEDS CHECK
+# x      <- np.array(DIMM)       : state vector
+# return -> np.matrix(DIMM,DIMM) : unit time tanjent linear matrix dM/dt
 def tangent_linear(x):
+  m = np.matrix(np.zeros((DIMM,DIMM)))
   if (DIMM == 3):
     sigma = 10.0
     r = 28.0
     b = 8.0 / 3.0
-    k = np.zeros((3,3))
 
-    k[0][0] = -sigma
-    k[0][1] = sigma
-    k[0][2] = 0.0
+    m[0][0] = -sigma
+    m[0][1] = sigma
+    m[0][2] = 0.0
 
-    k[1][0] = -x[2] + r
-    k[1][1] = -1.0
-    k[1][2] = -x[0]
+    m[1][0] = -x[2] + r
+    m[1][1] = -1.0
+    m[1][2] = -x[0]
 
-    k[2][0] = x[1]
-    k[2][1] = x[0]
-    k[2][2] = -b
-    return k
+    m[2][0] = x[1]
+    m[2][1] = x[0]
+    m[2][2] = -b
+
+  elif (DIMM == 9):
+    sigma = 10.0
+    r     = 28.0
+    b     = 8.0 / 3.0
+    tau   = 0.1
+    c     = 1.0
+    cz    = 1.0
+    ce    = 0.08
+    s     = 1.0
+    k1    = 10.0
+    k2    = -11.0
+
+    # extratropic atm
+    m[0][0] = -sigma
+    m[0][1] = sigma
+    m[0][3] = -ce * s
+
+    m[1][0] = -x[2] + r
+    m[1][1] = -1.0
+    m[1][2] = -x[0]
+    m[1][4] = ce * s
+
+    m[2][0] = x[1]
+    m[2][1] = x[0]
+    m[2][2] = -b
+
+    # tropic atm
+    m[3][0] = -ce * s
+    m[3][3] = -sigma
+    m[3][4] = sigma
+    m[3][6] = -c * s
+
+    m[4][1] = ce * s
+    m[4][3] = -x[5] + r
+    m[4][4] = -1.0
+    m[4][5] = -x[3]
+    m[4][7] = c * s
+
+    m[5][3] = x[4]
+    m[5][4] = x[3]
+    m[5][5] = -b
+    m[5][8] = cz
+
+    # tropic ocn
+    m[6][3] = - c
+    m[6][6] = tau * (-sigma)
+    m[6][7] = tau * sigma
+
+    m[7][4] = c
+    m[7][6] = tau * (-s * x[8] + r)
+    m[7][7] = -tau
+    m[7][8] = tau * (-s * x[6])
+
+    m[8][5] = -cz
+    m[8][6] = tau * (s * x[7])
+    m[8][7] = tau * (s * x[6])
+    m[8][8] = tau * (-b)
+
+  return m
 
