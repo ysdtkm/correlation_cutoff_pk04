b602abc8f3dac62d812236e2d7c80bb1d4dc4293
Takuma Yoshida <t.y.1025@gmail.com>
2017-02-16 17:49:44 -0500

    add lvs refs #313

--- works/couple_lorenz63/Py/vectors.py
+++ works/couple_lorenz63/Py/vectors.py
@@ -8,5 +8,14 @@ def orth_norm_vectors(lv, eps):
   # eps    <- float               : small size to normalize LVs
   # return -> np.array[DIMM,DIMM] : orthonormalized LVs in descending order
 
+  # need to check if the column vectors of q are unit length
   q, r = np.linalg.qr(lv)
+  lv0 = np.copy(lv)
+
+  eigvals = np.diag(r)
+  order = np.argsort(eigvals)
+  for i in range(DIMM):
+    lv[i,:] = lv0[order[i],:]
+    norm = np.sqrt(np.sum(np.power(lv[i,:], 2)))
+    lv[i,:] *= eps / norm
   return lv
