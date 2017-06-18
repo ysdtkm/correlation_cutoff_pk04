cfd329e091483c2f4070d20ff27cb0c5acc077c3
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 21:28:20 +0000

    mod

--- works/couple_lorenz63/aws/myjob.sh
+++ works/couple_lorenz63/aws/myjob.sh
@@ -3,12 +3,15 @@
 ls /tmp
 cd /tmp/repos/works/couple_lorenz63
 git pull
-make
+make plot
 
-cp -f latex/out.pdf image/
 commit=`git show HEAD | head -n1 | cut -c8-14`
 DATE=`date "+%Y%m%d_%H%M%S"`
 title="${DATE}_${commit}_${BATCH_JOB_NAME}"
-tar -czvf ${title}.tar.gz image
-aws s3 cp ${title}.tar.gz s3://ysdtkm-bucket-1/couple_lorenz63/tar/
+
+aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/${title} --recursive
+# tar -czvf ${title}.tar.gz image
+
+make tex
+aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/tar/${title}/
 aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/${title}.pdf
