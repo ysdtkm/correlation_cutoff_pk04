f1ba9ce70d67e873530a78d5ef7d59eea4be02f6
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 20:50:47 +0000

    mod

--- works/couple_lorenz63/aws/myjob.sh
+++ works/couple_lorenz63/aws/myjob.sh
@@ -8,6 +8,7 @@ make
 cp -f latex/out.pdf image/
 commit=`git show HEAD | head -n1 | cut -c8-14`
 DATE=`date "+%Y%m%d_%H%M%S"`
-tar -czvf ${DATE}_${commit}.tar.gz image
-aws s3 cp ${DATE}_${commit}.tar.gz s3://ysdtkm-bucket-1/couple_lorenz63/tar/
-aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/${DATE}_${commit}.pdf
+title="${DATE}_${commit}_${BATCH_JOB_NAME}"
+tar -czvf ${title}.tar.gz image
+aws s3 cp ${title}.tar.gz s3://ysdtkm-bucket-1/couple_lorenz63/tar/
+aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/${title}.pdf
