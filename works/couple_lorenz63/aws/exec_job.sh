6269869e88469feba5aa0753986e32db5b529008
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 23:32:43 +0000

    mod

--- works/couple_lorenz63/aws/exec_job.sh
+++ works/couple_lorenz63/aws/exec_job.sh
@@ -22,8 +22,8 @@ COMMIT=`git show HEAD | head -n1 | cut -c8-14`
 
 cat <<EOF > ./aws/env.json
 {
-  "vcpus": 2,
-  "memory": 2000,
+  "vcpus": 1,
+  "memory": 1000,
   "command": ["./myjob.sh"],
   "environment": [
     {
