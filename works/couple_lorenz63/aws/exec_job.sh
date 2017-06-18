c357dfb51aa5216c52ee4d0d9c4322eecc551213
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 21:04:17 +0000

    mod

--- works/couple_lorenz63/aws/exec_job.sh
+++ works/couple_lorenz63/aws/exec_job.sh
@@ -9,11 +9,44 @@ else
   exit 1
 fi
 
+echo -n "input job name: "
+read JOBNAME
+
+echo -n "choose queue type (default: slow): "
+read QUEUETYPE
+if [ "${QUEUETYPE}" != "" ]; then
+  queue="queue_fast"
+else
+  queue="queue_slow"
+fi
+
+cat <<EOF > ./aws/env.json
+{
+  "vcpus": 2,
+  "memory": 2000,
+  "command": ["./myjob.sh"],
+  "environment": [
+    {
+      "name": "BATCH_FILE_TYPE",
+      "value": "script"
+    },
+    {
+      "name": "BATCH_FILE_S3_URL",
+      "value": "s3://ysdtkm-bucket-1/myjob.sh"
+    },
+    {
+      "name": "BATCH_JOB_NAME",
+      "value": "${JOBNAME}"
+    }
+  ]
+}
+EOF
+
 aws s3 cp aws/myjob.sh s3://ysdtkm-bucket-1/
 DATE=`date "+%Y%m%d-%H%M%S"`
 id=`aws batch submit-job \
   --job-name batch_python_${DATE} \
-  --job-queue queue_slow \
+  --job-queue ${queue} \
   --job-definition def-with-other-image:9 \
   --container-overrides file://aws/env.json | grep jobId`
 
@@ -21,3 +54,5 @@ idcut=`python -c "import sys; print(sys.argv[2])" ${id}`
 
 echo "type following to obtain result"
 echo "aws batch describe-jobs --jobs ${idcut} | grep status"
+
+rm -f ./aws/env.json
