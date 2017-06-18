ef2c0e9b5b454de98af2262da2cc905414c83d15
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 19:27:15 +0000

    mod

---
+++ works/couple_lorenz63/aws/exec_job.sh
@@ -0,0 +1,12 @@
+aws s3 cp myjob.sh s3://ysdtkm-bucket-1/
+DATE=`date "+%Y%m%d-%H%M%S"`
+id=`aws batch submit-job \
+  --job-name batch_python_${DATE} \
+  --job-queue queue-non-ami \
+  --job-definition def-with-other-image:9 \
+  --container-overrides file://env.json | grep jobId`
+
+idcut=`python -c "import sys; print(sys.argv[2])" ${id}`
+
+echo "type following to obtain result"
+echo "aws batch describe-jobs --jobs ${idcut} | grep status"
