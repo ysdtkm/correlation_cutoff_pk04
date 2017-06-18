13af3b075e0fbd69034d82d5d7f9fa03a76e226c
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-18 20:52:11 +0000

    mod

--- works/couple_lorenz63/aws/exec_job.sh
+++ works/couple_lorenz63/aws/exec_job.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git s
 echo -n "input something if you want to avoid auto-commit: "
 read INPUT
@@ -11,7 +13,7 @@ aws s3 cp aws/myjob.sh s3://ysdtkm-bucket-1/
 DATE=`date "+%Y%m%d-%H%M%S"`
 id=`aws batch submit-job \
   --job-name batch_python_${DATE} \
-  --job-queue queue_fast \
+  --job-queue queue_slow \
   --job-definition def-with-other-image:9 \
   --container-overrides file://aws/env.json | grep jobId`
 
