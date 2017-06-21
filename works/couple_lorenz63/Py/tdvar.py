d369add693f1240949f4818042e9e22cb244c308
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-21 14:11:00 +0000

    tdvar b10_100time

--- works/couple_lorenz63/Py/tdvar.py
+++ works/couple_lorenz63/Py/tdvar.py
@@ -5,7 +5,7 @@ import sys
 from scipy.optimize import fmin_bfgs
 from const import *
 
-Amplitude_B = 1.2
+Amplitude_B = 1.0
 
 def tdvar(fcst, h, r, yo, i_s, i_e):
   # fcst   <- np.array[dimc]        : first guess
