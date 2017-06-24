ff5323b9dafcc93e138aabb0aef70af0df8dc3e0
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-06-24 02:00:28 +0000

    seek_fdvar_100time_climbc

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -37,41 +37,21 @@ EXPLIST = [ \
   # {"name":"fdvar_non_clim_int8", "aint":8, \
   #       "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b1", "aint":8, "amp_b":1.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b1", "aint":8, "amp_b":1.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b1", "aint":8, "amp_b":1.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"}, \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b2", "aint":8, "amp_b":2.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b2", "aint":8, "amp_b":2.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b2", "aint":8, "amp_b":2.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"}, \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b3", "aint":8, "amp_b":3.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b3", "aint":8, "amp_b":3.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b3", "aint":8, "amp_b":3.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"}, \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b5", "aint":8, "amp_b":5.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b5", "aint":8, "amp_b":5.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b5", "aint":8, "amp_b":5.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"}, \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b7", "aint":8, "amp_b":7.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b7", "aint":8, "amp_b":7.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b7", "aint":8, "amp_b":7.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"}, \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"}, \
   {"name":"fdvar_non_int8_b10", "aint":8, "amp_b":10.0, \
-        "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  {"name":"fdvar_weak_int8_b10", "aint":8, "amp_b":10.0, \
-        "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  {"name":"fdvar_strong_int8_b10", "aint":8, "amp_b":10.0, \
-        "nmem":1, "method":"4dvar", "couple":"strong"} \
+        "nmem":1, "method":"4dvar", "couple":"none", "bc":"climatology"} \
+  # {"name":"fdvar_weak_int8_b10", "aint":8, "amp_b":10.0, \
+  #       "nmem":1, "method":"4dvar", "couple":"weak"}, \
+  # {"name":"fdvar_strong_int8_b10", "aint":8, "amp_b":10.0, \
+  #       "nmem":1, "method":"4dvar", "couple":"strong"} \
 ]
 
 Calc_lv = False
