2ec1be7dc61263c259194700a838a1357378617c
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-07-03 01:48:55 +0000

    letkf, R localization, only for strongly coupled DA

--- works/couple_lorenz63/Py/etkf.py
+++ works/couple_lorenz63/Py/etkf.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.linalg import sqrtm
 from const import *
 
-def etkf(fcst, h_nda, r_nda, yo_nda, rho, nmem):
+def etkf(fcst, h_nda, r_nda, yo_nda, rho, nmem, localization=False):
   # fcst   <- np.array[nmem, dimc]
   # h_nda  <- np.array[DIMO, dimc]
   # r_nda  <- np.array[DIMO, DIMO]
@@ -49,11 +49,39 @@ def etkf(fcst, h_nda, r_nda, yo_nda, rho, nmem):
   xfpt = xfm - xf * I_1m
   ybpt = h * xfpt
   yb   = h * xf
-  pa   = (((nmem-1.0)/rho) * I_mm + ybpt.T * r.I * ybpt).I
-  wam  = np.matrix(sqrtm((nmem-1.0) * pa))
-  wa   = pa * ybpt.T * r.I * (yo - yb)
-  xapt = (xfm - xf * I_1m) * wam
-  xa   = xf + xfm * wa
-  xam  = xapt + xa * I_1m
-  return np.real(xam.T.A), (xfpt * xfpt.T).A, (xapt * xapt.T).A
+
+  if localization:
+    xai = np.matrix(np.zeros((DIMM, nmem)))
+    xapt = np.copy(xfpt)
+
+    for j in range(DIMM):
+      # step 3: need elaboration for real localization
+      localization_weight = np.matrix(np.identity(DIMM))
+      for ix in range(DIMM):
+        if ix != j:
+          localization_weight[ix, ix] *= 0.5
+      yol = yo[:,:]
+      ybl = yb[:,:]
+      ybptl = ybpt[:,:]
+      xfl = xf[j,:]
+      xfptl = xfpt[j,:]
+      rl = r[:,:]
+
+      # step 4-9
+      cl = ybptl.T * np.asmatrix(rl.I.A * localization_weight.A)
+      pal = (((nmem-1.0)/rho) * I_mm + cl * ybptl).I
+      waptl = np.matrix(sqrtm((nmem-1.0) * pal))
+      wal = pal * cl * (yol - ybl)
+      xail = xfl * I_1m + xfptl * (wal * I_1m + waptl)
+      xai[j,:] = xail[:,:]
+    return np.real(xai.T.A), (xfpt * xfpt.T).A, None
+
+  else:
+    pa   = (((nmem-1.0)/rho) * I_mm + ybpt.T * r.I * ybpt).I
+    wam  = np.matrix(sqrtm((nmem-1.0) * pa))
+    wa   = pa * ybpt.T * r.I * (yo - yb)
+    xapt = (xfm - xf * I_1m) * wam
+    xa   = xf + xfm * wa
+    xam  = xapt + xa * I_1m
+    return np.real(xam.T.A), (xfpt * xfpt.T).A, (xapt * xapt.T).A
 
--- works/couple_lorenz63/Py/main.py
+++ works/couple_lorenz63/Py/main.py
@@ -153,7 +153,7 @@ def analyze_one_window(fcst, fcst_pre, obs, h, r, settings, i_s=0, i_e=DIMM, bc=
 
   if (settings["method"] == "etkf"):
     anl[:,:], bf[:,:], ba[:,:] = \
-        etkf(fcst[:,:], h[:,:], r[:,:], yo[:,:], settings["rho"], settings["nmem"])
+        etkf(fcst[:,:], h[:,:], r[:,:], yo[:,:], settings["rho"], settings["nmem"], True)
   elif (settings["method"] == "3dvar"):
     anl[0,:] = tdvar(fcst[0,:].T, h[:,:], r[:,:], yo[:,:], i_s, i_e, settings["amp_b"])
     # anl[0,:] = tdvar_interpol(fcst[0,:].T, h[:,:], r[:,:], yo[:,:], i_s, i_e, settings["amp_b"]) # ttk
