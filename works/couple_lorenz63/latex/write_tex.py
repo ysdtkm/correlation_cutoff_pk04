764467d6393f227260a3b615ff96fe420a8d43e8
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-07-06 00:25:53 +0000

    100time_4mem_r_localization

--- works/couple_lorenz63/latex/write_tex.py
+++ works/couple_lorenz63/latex/write_tex.py
@@ -10,16 +10,15 @@ def main():
   last_commit = git_output.split()[1][:7]
   date = datetime.datetime.now()
   datestr = "%04d%02d%02d" % (date.year, date.month, date.day)
-  date_for_tex = datestr.replace("_", "\\_")
-  txt_out = header(date_for_tex, last_commit)
+  txt_out = header(datestr, last_commit)
   expname = os.environ.get("BATCH_JOB_NAME", "")
   commit_name = last_commit + " " + expname
 
   # rmse and lyapunov exponents
-  txt_out += figure_table("", ["../image/true/rmse_bar.pdf"], 1, 1, date_for_tex, commit_name)
-  txt_out += write_txt(date_for_tex, commit_name,
+  txt_out += figure_table("", ["../image/true/rmse_bar.pdf"], 1, 1, datestr, commit_name)
+  txt_out += write_txt(datestr, commit_name,
                         "../image/true/rmse.txt", "9pt", "rmse")
-  txt_out += write_txt(date_for_tex, commit_name,
+  txt_out += write_txt(datestr, commit_name,
                         "../data/lyapunov.txt", "5.4pt", "lyapunov exponents")
 
   # rmse-spread comparison
@@ -28,9 +27,9 @@ def main():
     for exp in const.EXPLIST:
       filelist.append("../image/%s/%s_%s_time.pdf" % (exp["name"], exp["name"], strx))
     if len(filelist) <= 9:
-      txt_out += figure_table(strx, filelist, 3, 3, date_for_tex, commit_name)
+      txt_out += figure_table(strx, filelist, 3, 3, datestr, commit_name)
     else:
-      txt_out += figure_table(strx, filelist, 4, 4, date_for_tex, commit_name)
+      txt_out += figure_table(strx, filelist, 4, 4, datestr, commit_name)
 
   # all figures
   for exp in const.EXPLIST:
@@ -41,10 +40,10 @@ def main():
                "extra_time", "trop_time", "ocean_time", ""]
     for imgname in imglist:
       filelist.append("../image/%s/%s_%s.pdf" % (exp["name"], exp["name"], imgname))
-    txt_out += figure_table(exp["name"], filelist, 4, 4, date_for_tex, commit_name)
+    txt_out += figure_table(exp["name"], filelist, 4, 4, datestr, commit_name)
 
   # conditions
-  txt_out += write_txt(date_for_tex, commit_name, "../Py/const.py", "6pt", "settings")
+  txt_out += write_txt(datestr, commit_name, "../Py/const.py", "6pt", "settings")
   md_raw = subprocess.getoutput('md5sum ../data/*').split("\n")
   md_raw.sort()
   md_txt = ""
@@ -57,7 +56,7 @@ def main():
   f.write(md_txt)
   f.close()
 
-  txt_out += write_txt(date_for_tex, commit_name, "./md5sum.txt", "9pt", "checksum")
+  txt_out += write_txt(datestr, commit_name, "./md5sum.txt", "9pt", "checksum")
 
   # footer, output and compile
   txt_out += footer()
@@ -96,8 +95,8 @@ def header(date, last_commit):
     \\begin{document}
     % \\maketitle
   """
-  header = header.replace('@@date@@', date)
-  header = header.replace('@@title@@', last_commit)
+  header = header.replace('@@date@@', sanitize(date))
+  header = header.replace('@@title@@', sanitize(last_commit))
   return textwrap.dedent(header[1:-1])
 
 def figure_table(expname, file_list, nx, ny, date, commit):
@@ -124,7 +123,7 @@ def figure_table(expname, file_list, nx, ny, date, commit):
     \\begin{figure}[h]
       \\flushleft
   """[1:-1]
-  content = content.replace('@@expname@@', date + " " + commit + " "  + expname.replace("_", "\\_"))
+  content = content.replace('@@expname@@', sanitize(date + " " + commit + " "  + expname))
 
   for iy in range(ny):
     for ix in range(nx):
@@ -132,11 +131,9 @@ def figure_table(expname, file_list, nx, ny, date, commit):
         \\begin{minipage}[b]{@@widthcell@@\\linewidth}
           \\centering
           \\includegraphicsmaybe[width=@@widthimg@@]{@@filename@@}
-          % \\subcaption{@@num@@}
         \\end{minipage}
         """[1:-1]
       tex_figure = tex_figure.replace('@@filename@@', file_list[ix + iy * nx])
-      tex_figure = tex_figure.replace('@@expname@@', expname)
       tex_figure = tex_figure.replace('@@widthcell@@', width_cell)
       tex_figure = tex_figure.replace('@@widthimg@@', width_img)
       content += tex_figure
@@ -163,7 +160,7 @@ def write_txt(date, commit, path, size, title):
     }
     \\end{frame}
   """[1:-1]
-  content = content.replace('@@expname@@', date + " " + commit + " " + title)
+  content = content.replace('@@expname@@', sanitize(date + " " + commit + " " + title))
   content = content.replace('@@filepath@@', path)
   content = content.replace('@@size@@', size)
 
