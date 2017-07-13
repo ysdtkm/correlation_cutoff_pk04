0fa9ebb90726e772dd709d3fa0411f572dbe1c17
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-07-13 16:13:08 +0000

    reference

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -8,7 +8,7 @@ DIMM = 9    # dimension of model variable n
 DIMO = DIMM # dimension of observation variable m
 
 DT = 0.01
-TMAX = 1
+TMAX = 10
 STEPS = int(TMAX / DT)
 STEP_FREE = STEPS // 4
 FCST_LT = 5
@@ -27,24 +27,23 @@ EXPLIST = [ \
   # {"name":"etkf_non_4mem", "rho":1.1, "nmem":4, "method":"etkf", "couple":"none", "bc":"persistent"}, \
   # {"name":"etkf_weak_4mem", "rho":1.1, "nmem":4, "method":"etkf", "couple":"weak"}, \
   # {"name":"etkf_strong_4mem", "rho":1.1, "nmem":4, "method":"etkf", "couple":"strong"}, \
-  # {"name":"etkf_non_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"none", "bc":"persistent"}, \
-  # {"name":"etkf_weak_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"weak"}, \
-  # {"name":"non_coupled", "rho":1.1, "nmem":100, "method":"etkf", "couple":"none", "bc":"independent"}, \
+  {"name":"etkf_non_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"none", "bc":"persistent", "r_local":None}, \
+  {"name":"etkf_weak_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"weak", "r_local":None}, \
   {"name":"full", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"full"}, \
-  {"name":"vertical", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
-  {"name":"horizontal", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
-  {"name":"3-components", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
-  {"name":"atmos_sees_ocean", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
-  {"name":"ocean_sees_atmos", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
-  {"name":"dynamical", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
+  # {"name":"vertical", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
+  # {"name":"horizontal", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
+  # {"name":"3-components", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
+  # {"name":"atmos_sees_ocean", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
+  # {"name":"ocean_sees_atmos", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
+  # {"name":"dynamical", "rho":1.1, "nmem":10, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
   # {"name":"tdvar_non_b2_ind", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"none", "bc":"independent"}, \
-  # {"name":"tdvar_non_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"none", "bc":"persistent"}, \
-  # {"name":"tdvar_weak_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"weak"}, \
-  # {"name":"tdvar_strong_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"strong"}, \
+  {"name":"tdvar_non_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"none", "bc":"persistent"}, \
+  {"name":"tdvar_weak_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"weak"}, \
+  {"name":"tdvar_strong_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"strong"}, \
   # {"name":"fdvar_non_b15_ind", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"none", "bc":"independent"}, \
-  # {"name":"fdvar_non_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  # {"name":"fdvar_weak_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  # {"name":"fdvar_strong_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"strong"}, \
+  {"name":"fdvar_non_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
+  {"name":"fdvar_weak_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"weak"}, \
+  {"name":"fdvar_strong_b15", "amp_b":1.5, "nmem":1, "method":"4dvar", "couple":"strong"}, \
 ]
 
 Calc_lv = False
