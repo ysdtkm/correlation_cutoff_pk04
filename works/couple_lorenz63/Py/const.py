7a9be0eb702cfce7d296e198ca948cbd6f1cdbd8
Sen-Shichi <sen_7@dummy.com>
2017-07-16 19:42:54 -0400

    comparison2b_10mem

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -26,15 +26,15 @@ amp_b_fdvar = 1.5
 EXPLIST = [ \
   # {"name":"etkf_non_10mem", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"none", "bc":"persistent", "r_local":None}, \
   # {"name":"etkf_weak_10mem", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"weak", "r_local":None}, \
-  {"name":"full", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"full"}, \
-  {"name":"atmos_sees_ocean", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
-  {"name":"ocean_sees_atmos", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
-  {"name":"vertical", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
-  # {"name":"band", "rho":rho, "nmem":nmem "method":"etkf", "couple":"strong", "r_local":"band"}, \
-  # {"name":"horizontal", "rho":rho, "nmem":nmem "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
-  # {"name":"trop_sees_ocean", "rho":rho, "nmem":nmem "method":"etkf", "couple":"strong", "r_local":"trop_sees_ocean"}, \
-  # {"name":"ocean_sees_trop", "rho":rho, "nmem":nmem "method":"etkf", "couple":"strong", "r_local":"ocean_sees_trop"}, \
-  # {"name":"3-components", "rho":rho, "nmem":nmem "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
+  # {"name":"full", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"full"}, \
+  # {"name":"atmos_sees_ocean", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
+  # {"name":"ocean_sees_atmos", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
+  # {"name":"vertical", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
+  # {"name":"band", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"band"}, \
+  {"name":"horizontal", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
+  {"name":"trop_sees_ocean", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"trop_sees_ocean"}, \
+  {"name":"ocean_sees_trop", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_trop"}, \
+  {"name":"3-components", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
   # {"name":"dynamical", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
   # {"name":"tdvar_non_b2_ind", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"none", "bc":"independent"}, \
   # {"name":"tdvar_non_b2", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"none", "bc":"persistent"}, \
