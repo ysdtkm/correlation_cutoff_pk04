a0677743cda62c85d0037811edaf0d9b054062b8
Takuma Yoshida <t.yoshida@met.kishou.go.jp>
2017-07-16 08:48:40 -0400

    100mem_rho101

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -29,13 +29,13 @@ EXPLIST = [ \
   # {"name":"etkf_strong_4mem", "rho":1.1, "nmem":4, "method":"etkf", "couple":"strong"}, \
   # {"name":"etkf_non_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"none", "bc":"persistent", "r_local":None}, \
   # {"name":"etkf_weak_10mem", "rho":1.1, "nmem":10, "method":"etkf", "couple":"weak", "r_local":None}, \
-  {"name":"full", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"full"}, \
-  {"name":"vertical", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
-  {"name":"horizontal", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
-  {"name":"3-components", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
-  {"name":"atmos_sees_ocean", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
-  {"name":"ocean_sees_atmos", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
-  {"name":"dynamical", "rho":1.02, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
+  {"name":"full", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"full"}, \
+  {"name":"vertical", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
+  {"name":"horizontal", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
+  {"name":"3-components", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
+  {"name":"atmos_sees_ocean", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
+  {"name":"ocean_sees_atmos", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
+  {"name":"dynamical", "rho":1.01, "nmem":100, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
   # {"name":"tdvar_non_b2_ind", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"none", "bc":"independent"}, \
   # {"name":"tdvar_non_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"none", "bc":"persistent"}, \
   # {"name":"tdvar_weak_b2", "amp_b":2.0, "nmem":1, "method":"3dvar", "couple":"weak"}, \
