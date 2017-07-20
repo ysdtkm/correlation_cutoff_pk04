5b169711dd5a6d565fe79bd59db9fdcaa51d67fb
Sen-Shichi <sen_7@dummy.com>
2017-07-20 14:32:52 -0400

    mod

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -24,29 +24,7 @@ amp_b_tdvar = 2.0
 amp_b_fdvar = 1.5
 
 EXPLIST = [ \
-  # {"name":"etkf_non_10mem", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"none", "bc":"persistent", "r_local":None}, \
-  # {"name":"etkf_weak_10mem", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"weak", "r_local":None}, \
-  # {"name":"full", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"full"}, \
-  # {"name":"band", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"band"}, \
-  {"name":"horizontal", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
-  # {"name":"vertical", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"vertical"}, \
-  # {"name":"atmos_sees_ocean", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"atmos_sees_ocean"}, \
-  # {"name":"ocean_sees_atmos", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_atmos"}, \
-  # {"name":"trop_sees_ocean", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"trop_sees_ocean"}, \
-  # {"name":"ocean_sees_trop", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"ocean_sees_trop"}, \
-  # {"name":"3-components", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"3-components"}, \
-  # {"name":"dynamical", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"dynamical"}, \
-  # {"name":"correlation", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"correlation"}, \
-  # {"name":"covariance", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"covariance"}, \
-  # {"name":"random", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"random"}, \
-  # {"name":"tdvar_non_b2_ind", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"none", "bc":"independent"}, \
-  # {"name":"tdvar_non_b2", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"none", "bc":"persistent"}, \
-  # {"name":"tdvar_weak_b2", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"weak"}, \
-  # {"name":"tdvar_strong_b2", "amp_b":amp_b_tdvar, "nmem":1, "method":"3dvar", "couple":"strong"}, \
-  # {"name":"fdvar_non_b15_ind", "amp_b":amp_b_fdvar, "nmem":1, "method":"4dvar", "couple":"none", "bc":"independent"}, \
-  # {"name":"fdvar_non_b15", "amp_b":amp_b_fdvar, "nmem":1, "method":"4dvar", "couple":"none", "bc":"persistent"}, \
-  # {"name":"fdvar_weak_b15", "amp_b":amp_b_fdvar, "nmem":1, "method":"4dvar", "couple":"weak"}, \
-  # {"name":"fdvar_strong_b15", "amp_b":amp_b_fdvar, "nmem":1, "method":"4dvar", "couple":"strong"}, \
+  {"name":"horizontal", "rho":1.1, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"horizontal"}, \
 ]
 
 Calc_lv = False
--- works/couple_lorenz63/aws/myjob.py
+++ works/couple_lorenz63/aws/myjob.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
-import os, subprocess, sys
+import os, subprocess, sys, re
 
 os.chdir("/tmp/repos/works/couple_lorenz63")
 commit = os.getenv("BATCH_COMMIT", "dummy")
 job_name = os.getenv("BATCH_JOB_NAME", "dummy_job")
+from_template = os.getenv("BATCH_FROM_TEMPLATE")
+
 subprocess.check_call(["git", "pull"])
 subprocess.check_call(["git", "checkout", commit])
-subprocess.check_call("make")
 
-os.system("cp -f data/lyapunov.txt image/true/")
-os.system("cp -f latex/out.pdf image/")
-os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/%s --recursive" % job_name)
-os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/%s.pdf" % job_name)
+if from_template == "True":
+  rhos = ["1.05", "1.1"]
+  for rho in rhos:
+    rf = open("aws/template_const.py", "r")
+    wf = open("Py/const.py", "w")
+    for line in rf:
+      wf.write(re.sub("<<param1>>", rho, line))
+    rf.close()
+    wf.close()
+
+    subprocess.check_call("make")
+    os.system("cp -f data/lyapunov.txt image/true/")
+    os.system("cp -f latex/out.pdf image/")
+    os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/%s_%s --recursive" % (job_name, rho))
+    os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/%s_%s.pdf" % (job_name, rho))
+    os.system("mv -f image imege_%s" % rho)
+
+  from Py import super_verif
+  os.system("mkdir -p image")
+  super_verif.verif(rhos)
+  os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/%s_all --recursive" % job_name)
+
+else:
+  subprocess.check_call("make")
+  os.system("cp -f data/lyapunov.txt image/true/")
+  os.system("cp -f latex/out.pdf image/")
+  os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/%s --recursive" % job_name)
+  os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/%s.pdf" % job_name)
 
