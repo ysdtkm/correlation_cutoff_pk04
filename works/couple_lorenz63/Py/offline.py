da37e3151e929e6d9bca163ccfbba79c5005869c
Sen-Shichi <sen_7@dummy.com>
2017-07-21 18:39:50 -0400

    devide unit_test into unit_test and offline

---
+++ works/couple_lorenz63/Py/offline.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from const import *
+from model import *
+from fdvar import *
+from main import exec_nature, exec_obs, exec_free_run, exec_assim_cycle
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+def obtain_climatology():
+  nstep = 100000
+  all_true = np.empty((nstep, DIMM))
+
+  np.random.seed(1)
+  true = np.random.normal(0.0, FERR_INI, DIMM)
+
+  for i in range(0, nstep):
+    true[:] = timestep(true[:], DT)
+    all_true[i,:] = true[:]
+  # all_true.tofile("data/true_for_clim.bin")
+
+  mean = np.mean(all_true[nstep//2:,:], axis=0)
+  print("mean")
+  print(mean)
+
+  mean2 = np.mean(all_true[nstep//2:,:]**2, axis=0)
+  stdv = np.sqrt(mean2 - mean**2)
+  print("stdv")
+  print(stdv)
+
+  return 0
+
+def obtain_tdvar_b():
+  np.random.seed(100000007*2)
+  nature = exec_nature()
+  obs = exec_obs(nature)
+  settings = {"name":"etkf_strong_int8",  "rho":1.1, "aint":8, "nmem":10, \
+              "method":"etkf", "couple":"strong"}
+  np.random.seed(100000007*3)
+  free = exec_free_run(settings)
+  anl  = exec_assim_cycle(settings, free, obs)
+  hist_bf = np.fromfile("data/%s_covr_back.bin" % settings["name"], np.float64)
+  hist_bf = hist_bf.reshape((STEPS, DIMM, DIMM))
+  mean_bf = np.nanmean(hist_bf[STEPS//2:, :, :], axis=0)
+  trace = np.trace(mean_bf)
+  mean_bf *= (DIMM / trace)
+
+  print("[ \\")
+  for i in range(DIMM):
+    print("[", end="")
+    for j in range(DIMM):
+      print("%12.9g" % mean_bf[i,j], end="")
+      if j < (DIMM - 1):
+        print(", ", end="")
+    if i < (DIMM - 1):
+      print("], \\")
+    else:
+      print("]  \\")
+      print("]")
+
+  return 0
+
+def print_two_dim_nparray(data, format="%12.9g"):
+  n = data.shape[0]
+  m = data.shape[1]
+  print("[ \\")
+  for i in range(n):
+    print("[", end="")
+    for j in range(m):
+      print(format % data[i,j], end="")
+      if j < (m - 1):
+        print(", ", end="")
+    if i < (n - 1):
+      print("], \\")
+    else:
+      print("]  \\")
+      print("]")
+
+def plot_matrix(data, name="", title="", color=plt.cm.bwr, xlabel="", ylabel="", logscale=False, linthresh=1e-3):
+  fig, ax = plt.subplots(1)
+  fig.subplots_adjust(left=0.12, right=0.95, bottom=0.12, top=0.92)
+  cmax = np.max(np.abs(data))
+  if logscale:
+    map1 = ax.pcolor(data, cmap=color, norm=colors.SymLogNorm(linthresh=linthresh*cmax))
+  else:
+    map1 = ax.pcolor(data, cmap=color)
+  if (color == plt.cm.bwr):
+    map1.set_clim(-1.0 * cmax, cmax)
+  x0,x1 = ax.get_xlim()
+  y0,y1 = ax.get_ylim()
+  ax.set_aspect(abs(x1-x0)/abs(y1-y0))
+  ax.set_xlabel(xlabel)
+  ax.set_ylabel(ylabel)
+  cbar = plt.colorbar(map1)
+  plt.title(title)
+  plt.gca().invert_yaxis()
+  plt.savefig("./matrix_%s_%s.png" % (name, title))
+  plt.close()
+  return 0
+
+def obtain_r2_etkf():
+  use_posterior = False
+
+  np.random.seed(100000007*2)
+  nature = exec_nature()
+  obs = exec_obs(nature)
+  settings = {"name":"etkf_strong_int8",  "rho":1.1, "nmem":10,
+              "method":"etkf", "couple":"strong", "r_local": "full"}
+  np.random.seed(100000007*3)
+  free = exec_free_run(settings)
+  anl  = exec_assim_cycle(settings, free, obs)
+
+  nmem = settings["nmem"]
+  hist_fcst = np.fromfile("data/%s_cycle.bin" % settings["name"], np.float64)
+  hist_fcst = hist_fcst.reshape((STEPS, nmem, DIMM))
+
+  r_ijt = np.empty((STEPS, DIMM, DIMM))
+  r_ijt[:,:,:] = np.nan
+  r2_ijt = np.empty((STEPS, DIMM, DIMM))
+  r2_ijt[:,:,:] = np.nan
+  cov_ijt = np.empty((STEPS, DIMM, DIMM))
+  cov_ijt[:,:,:] = np.nan
+  cov2_ijt = np.empty((STEPS, DIMM, DIMM))
+  cov2_ijt[:,:,:] = np.nan
+
+  for it in range(STEPS//2, STEPS):
+    if it % AINT == 0:
+      if use_posterior:
+        fcst = hist_fcst[it, :, :].copy()
+      else:
+        fcst = hist_fcst[it-AINT, :, :].copy()
+        for jt in range(AINT):
+          for k in range(nmem):
+            fcst[k, :] = timestep(fcst[k,:], DT)
+      for i in range(DIMM):
+        for j in range(DIMM):
+          # a38p40
+          vector_i = np.copy(fcst[:, i])
+          vector_j = np.copy(fcst[:, j])
+          vector_i[:] -= np.mean(vector_i)
+          vector_j[:] -= np.mean(vector_j)
+          numera = np.sum(vector_i * vector_j)
+          denomi = (np.sum(vector_i ** 2) * np.sum(vector_j ** 2)) ** 0.5
+          r_ijt[it, i, j] = numera / denomi
+          r2_ijt[it, i, j] = numera ** 2 / denomi ** 2
+          cov_ijt[it, i, j] = numera
+          cov2_ijt[it, i, j] = numera **2
+
+  r_ij = np.nanmean(r_ijt, axis=0)
+  r2_ij = np.nanmean(r2_ijt, axis=0)
+  cov_ij = np.nanmean(cov_ijt, axis=0)
+  cov2_ij = np.nanmean(cov2_ijt, axis=0)
+
+  plot_matrix(r_ij, title="R", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-2)
+  plot_matrix(r2_ij, title="R2", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-2)
+  plot_matrix(cov_ij, title="Cov", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-3)
+  plot_matrix(cov2_ij, title="Cov2", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-6)
+
+  print("r-squared")
+  matrix_nondiagonal_order(r2_ij)
+  print("covariance-squared")
+  matrix_nondiagonal_order(cov2_ij)
+  print("random")
+  mat_rand = np.random.randn(DIMM, DIMM)
+  matrix_nondiagonal_order(mat_rand)
+  return 0
+
+def matrix_nondiagonal_order(mat_ij):
+  n = len(mat_ij)
+  if len(mat_ij[0]) != n:
+    raise Exception("input matrix non-square")
+
+  nondiagonal_components = []
+  for i in range(n):
+    for j in range(i+1, n):
+      nondiagonal_components.append((i, j, mat_ij[i,j]))
+  order = sorted(nondiagonal_components, key=lambda x: x[2], reverse=True)
+
+  def find_order(k, l):
+    i = min(k, l)
+    j = max(k, l)
+    for io, cmp in enumerate(order):
+      if cmp[0] == i and cmp[1] == j:
+        return io
+    raise Exception("find_order overflow")
+
+  for i in range(n):
+    for j in range(n):
+      if i == j:
+        print("-- ", end="")
+      else:
+        odr = find_order(i, j)
+        if odr < 14:
+          print("%02d " % odr, end="")
+        else:
+          print("** ", end="")
+    print("")
+  print("")
+
+  return 0
+
+obtain_r2_etkf()
--- works/couple_lorenz63/Py/unit_test.py
+++ works/couple_lorenz63/Py/unit_test.py
@@ -127,57 +127,40 @@ def test_cost_function_grad():
 
   return 0
 
-def obtain_climatology():
-  nstep = 100000
-  all_true = np.empty((nstep, DIMM))
+def check_b():
+  # obtained by unit_test.py/obtain_tdvar_b(), a66aa31, 100000 timesteps
+  new = np.array([ \
+    [  29.1361601,   40.8007041, 0.0996813749, 0.0791912453,  0.344864776, -0.136810502, 0.00336997879, 0.0318867672, 0.0437722107], \
+    [  40.8007041,   72.4114206,   0.90879686,  0.159329634,  0.564511249, -0.195910748, 0.00951459272, 0.0774520914, 0.0487791362], \
+    [0.0996813749,   0.90879686,   56.1769923, 0.0200836456, 0.0333094552, 0.00326933485, 0.00874575146, -0.00783791119, 0.0750019486], \
+    [0.0791912453,  0.159329634, 0.0200836456,   4.82189397,   5.65114453,  -1.76995579, -0.189879111,    1.8832435,  0.619351491], \
+    [ 0.344864776,  0.564511249, 0.0333094552,   5.65114453,   10.1145373,  0.202050997,  0.339100741,   2.79224997,  0.648284682], \
+    [-0.136810502, -0.195910748, 0.00326933485,  -1.76995579,  0.202050997,    11.011319,  -0.20636934,  -1.39320264, 0.0335684046], \
+    [0.00336997879, 0.00951459272, 0.00874575146, -0.189879111,  0.339100741,  -0.20636934,   3.56934671,   7.00919827,   2.82018057], \
+    [0.0318867672, 0.0774520914, -0.00783791119,    1.8832435,   2.79224997,  -1.39320264,   7.00919827,   26.5619341,   1.63512268], \
+    [0.0437722107, 0.0487791362, 0.0750019486,  0.619351491,  0.648284682, 0.0335684046,   2.82018057,   1.63512268,   19.8071293]  \
+  ])
 
-  np.random.seed(1)
-  true = np.random.normal(0.0, FERR_INI, DIMM)
+  # not known how it was obtained. trace ~= 127
+  old = np.array( \
+    [[1.55369946e-02, 2.59672759e-02, 2.14143702e-02, 5.50608636e-05, 2.06837903e-04, 9.53104122e-05, 4.01854229e-05, 1.10394688e-04, 1.10795043e-04], \
+     [2.59672759e-02, 4.60323124e-02, 3.22253391e-02, 9.67994015e-05, 3.47479991e-04, 1.59885557e-04, 6.25825607e-05, 1.71056662e-04, 1.75287574e-04], \
+     [2.14143702e-02, 3.22253391e-02, 4.49870690e-02, 8.07242543e-05, 2.90997346e-04, 1.68933576e-04, 7.17501951e-05, 1.91677973e-04, 2.08898907e-04], \
+     [5.50608636e-05, 9.67994015e-05, 8.07242543e-05, 1.19388552e-04, 1.32465565e-04, 1.41518295e-04, 1.26069316e-04, 3.89377305e-04, 5.26723750e-04], \
+     [2.06837903e-04, 3.47479991e-04, 2.90997346e-04, 1.32465565e-04, 2.19079467e-04, 1.28264891e-04, 2.15409763e-04, 5.82014129e-04, 7.50531420e-04], \
+     [9.53104122e-05, 1.59885557e-04, 1.68933576e-04, 1.41518295e-04, 1.28264891e-04, 3.00276052e-04, 2.90918095e-04, 8.40635972e-04, 8.63515366e-04], \
+     [4.01854229e-05, 6.25825607e-05, 7.17501951e-05, 1.26069316e-04, 2.15409763e-04, 2.90918095e-04, 9.88628471e-04, 2.56406108e-03, 2.73643907e-03], \
+     [1.10394688e-04, 1.71056662e-04, 1.91677973e-04, 3.89377305e-04, 5.82014129e-04, 8.40635972e-04, 2.56406108e-03, 8.41665768e-03, 6.19483319e-03], \
+     [1.10795043e-04, 1.75287574e-04, 2.08898907e-04, 5.26723750e-04, 7.50531420e-04, 8.63515366e-04, 2.73643907e-03, 6.19483319e-03, 1.08456084e-02]] \
+  ) * 1000
 
-  for i in range(0, nstep):
-    true[:] = timestep(true[:], DT)
-    all_true[i,:] = true[:]
-  # all_true.tofile("data/true_for_clim.bin")
+  print_two_dim_nparray(old / new * 10000, "%8.4g")
+  print_two_dim_nparray(new, "%8.4g")
+  print(np.trace(old))
 
-  mean = np.mean(all_true[nstep//2:,:], axis=0)
-  print("mean")
-  print(mean)
-
-  mean2 = np.mean(all_true[nstep//2:,:]**2, axis=0)
-  stdv = np.sqrt(mean2 - mean**2)
-  print("stdv")
-  print(stdv)
-
-  return 0
-
-def obtain_tdvar_b():
-  np.random.seed(100000007*2)
-  nature = exec_nature()
-  obs = exec_obs(nature)
-  settings = {"name":"etkf_strong_int8",  "rho":1.1, "aint":8, "nmem":10, \
-              "method":"etkf", "couple":"strong"}
-  np.random.seed(100000007*3)
-  free = exec_free_run(settings)
-  anl  = exec_assim_cycle(settings, free, obs)
-  hist_bf = np.fromfile("data/%s_covr_back.bin" % settings["name"], np.float64)
-  hist_bf = hist_bf.reshape((STEPS, DIMM, DIMM))
-  mean_bf = np.nanmean(hist_bf[STEPS//2:, :, :], axis=0)
-  trace = np.trace(mean_bf)
-  mean_bf *= (DIMM / trace)
-
-  print("[ \\")
-  for i in range(DIMM):
-    print("[", end="")
-    for j in range(DIMM):
-      print("%12.9g" % mean_bf[i,j], end="")
-      if j < (DIMM - 1):
-        print(", ", end="")
-    if i < (DIMM - 1):
-      print("], \\")
-    else:
-      print("]  \\")
-      print("]")
-
+  plot_matrix(np.log(np.abs(old / new)), "", "old_over_new", color=plt.cm.Reds)
+  plot_matrix(np.log(np.abs(old)), "", "old", color=plt.cm.Reds)
+  plot_matrix(np.log(np.abs(new)), "", "new", color=plt.cm.Reds)
   return 0
 
 def print_two_dim_nparray(data, format="%12.9g"):
@@ -216,42 +199,6 @@ def plot_matrix(data, name="", title="", color=plt.cm.bwr, xlabel="", ylabel="",
   plt.gca().invert_yaxis()
   plt.savefig("./matrix_%s_%s.png" % (name, title))
   plt.close()
-  return 0
-
-def check_b():
-  # obtained by unit_test.py/obtain_tdvar_b(), a66aa31, 100000 timesteps
-  new = np.array([ \
-    [  29.1361601,   40.8007041, 0.0996813749, 0.0791912453,  0.344864776, -0.136810502, 0.00336997879, 0.0318867672, 0.0437722107], \
-    [  40.8007041,   72.4114206,   0.90879686,  0.159329634,  0.564511249, -0.195910748, 0.00951459272, 0.0774520914, 0.0487791362], \
-    [0.0996813749,   0.90879686,   56.1769923, 0.0200836456, 0.0333094552, 0.00326933485, 0.00874575146, -0.00783791119, 0.0750019486], \
-    [0.0791912453,  0.159329634, 0.0200836456,   4.82189397,   5.65114453,  -1.76995579, -0.189879111,    1.8832435,  0.619351491], \
-    [ 0.344864776,  0.564511249, 0.0333094552,   5.65114453,   10.1145373,  0.202050997,  0.339100741,   2.79224997,  0.648284682], \
-    [-0.136810502, -0.195910748, 0.00326933485,  -1.76995579,  0.202050997,    11.011319,  -0.20636934,  -1.39320264, 0.0335684046], \
-    [0.00336997879, 0.00951459272, 0.00874575146, -0.189879111,  0.339100741,  -0.20636934,   3.56934671,   7.00919827,   2.82018057], \
-    [0.0318867672, 0.0774520914, -0.00783791119,    1.8832435,   2.79224997,  -1.39320264,   7.00919827,   26.5619341,   1.63512268], \
-    [0.0437722107, 0.0487791362, 0.0750019486,  0.619351491,  0.648284682, 0.0335684046,   2.82018057,   1.63512268,   19.8071293]  \
-  ])
-
-  # not known how it was obtained. trace ~= 127
-  old = np.array( \
-    [[1.55369946e-02, 2.59672759e-02, 2.14143702e-02, 5.50608636e-05, 2.06837903e-04, 9.53104122e-05, 4.01854229e-05, 1.10394688e-04, 1.10795043e-04], \
-     [2.59672759e-02, 4.60323124e-02, 3.22253391e-02, 9.67994015e-05, 3.47479991e-04, 1.59885557e-04, 6.25825607e-05, 1.71056662e-04, 1.75287574e-04], \
-     [2.14143702e-02, 3.22253391e-02, 4.49870690e-02, 8.07242543e-05, 2.90997346e-04, 1.68933576e-04, 7.17501951e-05, 1.91677973e-04, 2.08898907e-04], \
-     [5.50608636e-05, 9.67994015e-05, 8.07242543e-05, 1.19388552e-04, 1.32465565e-04, 1.41518295e-04, 1.26069316e-04, 3.89377305e-04, 5.26723750e-04], \
-     [2.06837903e-04, 3.47479991e-04, 2.90997346e-04, 1.32465565e-04, 2.19079467e-04, 1.28264891e-04, 2.15409763e-04, 5.82014129e-04, 7.50531420e-04], \
-     [9.53104122e-05, 1.59885557e-04, 1.68933576e-04, 1.41518295e-04, 1.28264891e-04, 3.00276052e-04, 2.90918095e-04, 8.40635972e-04, 8.63515366e-04], \
-     [4.01854229e-05, 6.25825607e-05, 7.17501951e-05, 1.26069316e-04, 2.15409763e-04, 2.90918095e-04, 9.88628471e-04, 2.56406108e-03, 2.73643907e-03], \
-     [1.10394688e-04, 1.71056662e-04, 1.91677973e-04, 3.89377305e-04, 5.82014129e-04, 8.40635972e-04, 2.56406108e-03, 8.41665768e-03, 6.19483319e-03], \
-     [1.10795043e-04, 1.75287574e-04, 2.08898907e-04, 5.26723750e-04, 7.50531420e-04, 8.63515366e-04, 2.73643907e-03, 6.19483319e-03, 1.08456084e-02]] \
-  ) * 1000
-
-  print_two_dim_nparray(old / new * 10000, "%8.4g")
-  print_two_dim_nparray(new, "%8.4g")
-  print(np.trace(old))
-
-  plot_matrix(np.log(np.abs(old / new)), "", "old_over_new", color=plt.cm.Reds)
-  plot_matrix(np.log(np.abs(old)), "", "old", color=plt.cm.Reds)
-  plot_matrix(np.log(np.abs(new)), "", "new", color=plt.cm.Reds)
   return 0
 
 def compare_coupled_vs_persistent_bc():
@@ -307,105 +254,3 @@ def compare_coupled_vs_persistent_bc():
   plt.ylabel("RMSD, coupled vs persistent BC forecasts")
   plt.savefig("./rmsd_coupled_vs_persistentbc.png")
 
-def obtain_r2_etkf():
-  use_posterior = False
-
-  np.random.seed(100000007*2)
-  nature = exec_nature()
-  obs = exec_obs(nature)
-  settings = {"name":"etkf_strong_int8",  "rho":1.1, "nmem":10,
-              "method":"etkf", "couple":"strong", "r_local": "full"}
-  np.random.seed(100000007*3)
-  free = exec_free_run(settings)
-  anl  = exec_assim_cycle(settings, free, obs)
-
-  nmem = settings["nmem"]
-  hist_fcst = np.fromfile("data/%s_cycle.bin" % settings["name"], np.float64)
-  hist_fcst = hist_fcst.reshape((STEPS, nmem, DIMM))
-
-  r_ijt = np.empty((STEPS, DIMM, DIMM))
-  r_ijt[:,:,:] = np.nan
-  r2_ijt = np.empty((STEPS, DIMM, DIMM))
-  r2_ijt[:,:,:] = np.nan
-  cov_ijt = np.empty((STEPS, DIMM, DIMM))
-  cov_ijt[:,:,:] = np.nan
-  cov2_ijt = np.empty((STEPS, DIMM, DIMM))
-  cov2_ijt[:,:,:] = np.nan
-
-  for it in range(STEPS//2, STEPS):
-    if it % AINT == 0:
-      if use_posterior:
-        fcst = hist_fcst[it, :, :].copy()
-      else:
-        fcst = hist_fcst[it-AINT, :, :].copy()
-        for jt in range(AINT):
-          for k in range(nmem):
-            fcst[k, :] = timestep(fcst[k,:], DT)
-      for i in range(DIMM):
-        for j in range(DIMM):
-          # a38p40
-          vector_i = np.copy(fcst[:, i])
-          vector_j = np.copy(fcst[:, j])
-          vector_i[:] -= np.mean(vector_i)
-          vector_j[:] -= np.mean(vector_j)
-          numera = np.sum(vector_i * vector_j)
-          denomi = (np.sum(vector_i ** 2) * np.sum(vector_j ** 2)) ** 0.5
-          r_ijt[it, i, j] = numera / denomi
-          r2_ijt[it, i, j] = numera ** 2 / denomi ** 2
-          cov_ijt[it, i, j] = numera
-          cov2_ijt[it, i, j] = numera **2
-
-  r_ij = np.nanmean(r_ijt, axis=0)
-  r2_ij = np.nanmean(r2_ijt, axis=0)
-  cov_ij = np.nanmean(cov_ijt, axis=0)
-  cov2_ij = np.nanmean(cov2_ijt, axis=0)
-
-  plot_matrix(r_ij, title="R", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-2)
-  plot_matrix(r2_ij, title="R2", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-2)
-  plot_matrix(cov_ij, title="Cov", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-3)
-  plot_matrix(cov2_ij, title="Cov2", xlabel="grid index i", ylabel="grid index j", logscale=True, linthresh=1e-6)
-
-  print("r-squared")
-  matrix_nondiagonal_order(r2_ij)
-  print("covariance-squared")
-  matrix_nondiagonal_order(cov2_ij)
-  print("random")
-  mat_rand = np.random.randn(DIMM, DIMM)
-  matrix_nondiagonal_order(mat_rand)
-  return 0
-
-def matrix_nondiagonal_order(mat_ij):
-  n = len(mat_ij)
-  if len(mat_ij[0]) != n:
-    raise Exception("input matrix non-square")
-
-  nondiagonal_components = []
-  for i in range(n):
-    for j in range(i+1, n):
-      nondiagonal_components.append((i, j, mat_ij[i,j]))
-  order = sorted(nondiagonal_components, key=lambda x: x[2], reverse=True)
-
-  def find_order(k, l):
-    i = min(k, l)
-    j = max(k, l)
-    for io, cmp in enumerate(order):
-      if cmp[0] == i and cmp[1] == j:
-        return io
-    raise Exception("find_order overflow")
-
-  for i in range(n):
-    for j in range(n):
-      if i == j:
-        print("-- ", end="")
-      else:
-        odr = find_order(i, j)
-        if odr < 14:
-          print("%02d " % odr, end="")
-        else:
-          print("** ", end="")
-    print("")
-  print("")
-
-  return 0
-
-obtain_r2_etkf()
