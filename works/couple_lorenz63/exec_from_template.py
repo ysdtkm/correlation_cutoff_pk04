36d8da4edfdf08baec121d7516463b2fb16a73a8
Sen-Shichi <sen_7@dummy.com>
2017-07-26 16:33:16 -0400

    test13_pdf_dir

--- works/couple_lorenz63/exec_from_template.py
+++ works/couple_lorenz63/exec_from_template.py
@@ -25,7 +25,7 @@ def exec_not_from_template(job_name, flag_local):
   os.system("cp -f latex/out.pdf image/")
   if not flag_local:
     os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/%s --recursive" % job_name)
-    os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/%s.pdf" % job_name)
+    os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/pdf/%s.pdf" % job_name)
 
 def exec_from_template(param1s, param2s, param3s, job_name, flag_local):
   for param1 in param1s:
@@ -40,7 +40,7 @@ def exec_from_template(param1s, param2s, param3s, job_name, flag_local):
       if not flag_local:
         os.system("aws s3 cp image s3://ysdtkm-bucket-1/couple_lorenz63/tar/%s/%s_%s --recursive"
           % (job_name, param1, param2))
-        os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/%s/%s_%s.pdf"
+        os.system("aws s3 cp latex/out.pdf s3://ysdtkm-bucket-1/couple_lorenz63/pdf/%s/%s_%s.pdf"
           % (job_name, param1, param2))
       os.system("rm -rf image_%s_%s" % (param1, param2))
       os.system("mv image image_%s_%s" % (param1, param2))
@@ -50,7 +50,7 @@ def exec_from_template(param1s, param2s, param3s, job_name, flag_local):
   os.system("mkdir -p verif")
   super_verif.verif(param1s, param2s, param3s)
   if not flag_local:
-    os.system("aws s3 sync verif s3://ysdtkm-bucket-1/couple_lorenz63/%s" % job_name)
+    os.system("aws s3 sync verif s3://ysdtkm-bucket-1/couple_lorenz63/pdf/%s" % job_name)
 
 def write_const_file_from_template(param1, param2, param3s):
   rf = open("aws/template_const.py", "r")
