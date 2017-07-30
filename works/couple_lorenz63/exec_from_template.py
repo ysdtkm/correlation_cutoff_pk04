a39ea2a988e8f1fd2afc911edd059dcb25698dd0
Sen-Shichi <sen_7@dummy.com>
2017-07-30 12:44:14 -0400

    covariance_rms_vs_mean_vs_clim

--- works/couple_lorenz63/exec_from_template.py
+++ works/couple_lorenz63/exec_from_template.py
@@ -6,12 +6,11 @@ import numpy as np
 # ====================================
 from_template = True
 if from_template:
-  param1s = ["4"]
-  param2s = ["covariance-clim"]
+  param1s = ["4", "5", "6"]
+  param2s = ["covariance-rms", "covariance-mean", "covariance-clim"]
   # param2s = ["correlation-rms", "correlation-mean", "covariance-rms", "covariance-mean",
   #            "BHHtRi-mean", "BHHtRi-rms", "covariance-clim", "correlation-clim"]
-  param3s = ["9", "18", "25", "27"]
-  # param3s = list(map(str, range(9, 82)))
+  param3s = list(map(str, range(9, 82)))
 # ====================================
 
 def main():
