18f6905399dd46dc1bbcc87799ff65544a85d89c
Sen-Shichi <sen_7@dummy.com>
2017-08-02 13:52:18 -0400

    mod

---
+++ works/couple_lorenz63/halo_submit.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+git status --short --branch .
+if [ $# -lt 1 ]; then
+  echo "Type (Ctrl-C) to stop."
+  echo ""
+  echo -n "input JOB NAME: "
+  read JOBNAME
+else
+  echo "Type Enter to continue. Type (Ctrl-C) to stop."
+  read DUMMY
+  JOBNAME=$1
+fi
+
+set +e
+git add .
+git commit -m "${JOBNAME}"
+set -e
+
+git pull && git push
+echo ""
+COMMIT=`git show HEAD | head -n1 | cut -c8-14`
+DATE=`date "+%Y%m%d_%H%M"`
+JOBNAME=${DATE}_${COMMIT}_${JOBNAME}
+WDIR=/homes/metogra/tyoshida/shrt/submit
+WDIR_IN_REPOS=works/couple_lorenz63
+COMMANDS="./exec_from_template.py"
+# COMMANDS="make clean all"
+
+ssh tyoshida@halo.atmos.umd.edu -t "nohup bash /homes/metogra/tyoshida/repos/works/submit_halo/exec_halo.sh ${COMMIT} ${JOBNAME} \$\$ ${WDIR} ${WDIR_IN_REPOS} ${COMMANDS} >& ~/shrt/log/${JOBNAME}_\$\$.log &"
