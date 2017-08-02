22c5f380e332f36e303c7f4f384156eebbdfd907
Sen-Shichi <sen_7@dummy.com>
2017-08-02 13:55:20 -0400

    test_halo

--- works/couple_lorenz63/halo_submit.sh
+++ works/couple_lorenz63/halo_submit.sh
@@ -22,10 +22,10 @@ git pull && git push
 echo ""
 COMMIT=`git show HEAD | head -n1 | cut -c8-14`
 DATE=`date "+%Y%m%d_%H%M"`
-JOBNAME=${DATE}_${COMMIT}_${JOBNAME}
+JOBNAME2=${DATE}_${COMMIT}_${JOBNAME}
 WDIR=/homes/metogra/tyoshida/shrt/submit
 WDIR_IN_REPOS=works/couple_lorenz63
 COMMANDS="./exec_from_template.py"
 # COMMANDS="make clean all"
 
-ssh tyoshida@halo.atmos.umd.edu -t "nohup bash /homes/metogra/tyoshida/repos/works/submit_halo/exec_halo.sh ${COMMIT} ${JOBNAME} \$\$ ${WDIR} ${WDIR_IN_REPOS} ${COMMANDS} >& ~/shrt/log/${JOBNAME}_\$\$.log &"
+ssh tyoshida@halo.atmos.umd.edu -t "nohup bash /homes/metogra/tyoshida/repos/works/submit_halo/exec_halo.sh ${COMMIT} ${JOBNAME2} \$\$ ${WDIR} ${WDIR_IN_REPOS} ${COMMANDS} >& ~/shrt/log/${JOBNAME2}_\$\$.log &"
