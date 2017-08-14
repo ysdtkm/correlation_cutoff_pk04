6a68e87bf324bb85d91f307eb7d6c0b436d3eabc
Sen-Shichi <sen_7@dummy.com>
2017-08-14 10:18:32 -0400

    reference for changing DIMO

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -8,7 +8,7 @@ DIMM = 9    # dimension of model variable n
 DIMO = DIMM # dimension of observation variable m
 
 DT = 0.01
-TMAX = 10
+TMAX = 2
 STEPS = int(TMAX / DT)
 STEP_FREE = STEPS // 4
 FCST_LT = 0
@@ -18,19 +18,26 @@ OERR_O = 5.0
 FERR_INI = 10.0
 AINT = 8
 
-rho = "adaptive"
+rho = 1.05 # "adaptive"
 nmem = 6
 amp_b_tdvar = 2.0
 amp_b_fdvar = 1.5
 
 EXPLIST = [ \
-  {"name":"covariance-rms_10", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"covariance-rms", "num_yes":10},
-  {"name":"covariance-rms_81", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"covariance-rms", "num_yes":81},
+  {"name":"etkf_strong", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"full"},
+  {"name":"tdvar_strong", "nmem":1, "method":"3dvar", "couple":"strong", "amp_b":amp_b_tdvar},
+  {"name":"fdvar_strong", "nmem":1, "method":"4dvar", "couple":"strong", "amp_b":amp_b_fdvar},
+  {"name":"etkf_weak", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"weak", "r_local":"full"},
+  {"name":"tdvar_weak", "nmem":1, "method":"3dvar", "couple":"weak", "amp_b":amp_b_tdvar},
+  {"name":"fdvar_weak", "nmem":1, "method":"4dvar", "couple":"weak", "amp_b":amp_b_fdvar},
+  {"name":"etkf_non", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"none", "r_local":"full", "bc":"persistence"},
+  {"name":"tdvar_non", "nmem":1, "method":"3dvar", "couple":"none", "amp_b":amp_b_tdvar, "bc":"persistence"},
+  {"name":"fdvar_non", "nmem":1, "method":"4dvar", "couple":"none", "amp_b":amp_b_fdvar, "bc":"persistence"},
 ]
 
 # {"name":"etkf", "rho":rho, "nmem":nmem, "method":"etkf", "couple":"strong", "r_local":"full"},
-# {"name":"tdvar", "rho":rho, "nmem":1, "method":"3dvar", "couple":"strong", "amp_b":amp_b_tdvar},
-# {"name":"fdvar", "rho":rho, "nmem":1, "method":"4dvar", "couple":"strong", "amp_b":amp_b_fdvar},
+# {"name":"tdvar", "nmem":1, "method":"3dvar", "couple":"strong", "amp_b":amp_b_tdvar},
+# {"name":"fdvar", "nmem":1, "method":"4dvar", "couple":"strong", "amp_b":amp_b_fdvar},
 
 Calc_lv = False
 
