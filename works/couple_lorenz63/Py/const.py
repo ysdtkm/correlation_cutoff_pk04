b92cf175da4f72989f69ae7ab56b44f736954678
Sen-Shichi <sen_7@dummy.com>
2017-08-16 20:43:38 -0400

    4mem_z

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -81,8 +81,8 @@ def geth() -> np.ndarray:
     #     warnings.warn("geth() cannot correctly deal with P_OBS != N_MODEL. P_OBS=%d, N_MODEL=%d was passed."
     #                   % (P_OBS, N_MODEL))
     # ttk
-    h = np.diag([0, 1, 0, 0, 1, 0, 0, 1, 0])  # y-only
-    # h = np.diag([0, 0, 1, 0, 0, 1, 0, 0, 1])  # z-only
+    # h = np.diag([0, 1, 0, 0, 1, 0, 0, 1, 0])  # y-only
+    h = np.diag([0, 0, 1, 0, 0, 1, 0, 0, 1])  # z-only
     # h = np.diag([1, 1, 1, 1, 1, 1, 1, 1, 1])
     return h
 
