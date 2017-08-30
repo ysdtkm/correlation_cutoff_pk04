99b7611dda62491466f4f1de296b727bcf9d62cd
Sen-Shichi <sen_7@dummy.com>
2017-08-30 18:59:26 -0400

    test6

--- works/couple_lorenz63/halo_submit.sh
+++ works/couple_lorenz63/halo_submit.sh
@@ -28,7 +28,7 @@ WDIR_IN_REPOS=works/couple_lorenz63
 SAVE_TAR="tar"
 SAVE_RAW="raw"
 
-# COMMANDS="./exec_from_template.py ${JOBNAME2}"
-COMMANDS="make clean all"
+COMMANDS="./exec_from_template.py ${JOBNAME2}"
+# COMMANDS="make clean all"
 
 ssh tyoshida@halo.atmos.umd.edu -t "nohup bash /homes/metogra/tyoshida/repos/works/submit_halo/exec_halo.sh ${COMMIT} ${JOBNAME2} \$\$ ${WDIR} ${WDIR_IN_REPOS} ${SAVE_TAR} ${SAVE_RAW} ${COMMANDS} >& ~/shrt/log/${JOBNAME2}_\$\$.log &"
