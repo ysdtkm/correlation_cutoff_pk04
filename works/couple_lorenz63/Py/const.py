e9f25881788e9b80b5185d4dabc7c91f87c5129c
Sen-Shichi <sen_7@dummy.com>
2017-08-31 00:21:45 -0400

    mod

--- works/couple_lorenz63/Py/const.py
+++ works/couple_lorenz63/Py/const.py
@@ -12,7 +12,7 @@ N_ATM = 6
 P_ATM = N_ATM
 
 DT = 0.01
-TMAX = 1000
+TMAX = 10
 STEPS = int(TMAX / DT)
 STEP_FREE = STEPS // 4
 FCST_LT = 0
@@ -28,7 +28,7 @@ amp_b_tdvar = 2.0
 amp_b_fdvar = 1.5
 
 EXPLIST = [ \
-  {"name":"covariance-rms_81", "rho":rho, "nmem":6, "method":"etkf", "couple":"strong", "r_local":"covariance-rms", "num_yes":81},
+  {"name":"correlation-rms_81", "rho":rho, "nmem":4, "method":"etkf", "couple":"strong", "r_local":"correlation-rms", "num_yes":81},
 ]
 
 # dict(name="etkf", rho=rho, nmem=nmem, method="etkf", couple="strong", r_local="full"),
