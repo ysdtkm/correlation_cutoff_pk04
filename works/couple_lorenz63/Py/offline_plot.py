1f159784fb1fd14f64144e3066eec9d7c0067d50
Sen-Shichi <sen_7@dummy.com>
2017-10-09 12:39:29 -0400

    mod

--- works/couple_lorenz63/Py/offline_plot.py
+++ works/couple_lorenz63/Py/offline_plot.py
@@ -149,7 +149,7 @@ def plot_lagged_correlation():
 
         plt.rcParams["font.size"] = 20
         fig, ax = plt.subplots()
-        fig.subplots_adjust(left=0.13, right=0.97, bottom=0.15, top=0.95)
+        fig.subplots_adjust(left=0.15, right=0.97, bottom=0.15, top=0.95)
         x = np.array(range(1, len(sorted_vals) + 1))
         y = np.array(sorted_vals)
         plt.bar(x, y / np.max(y), label=name, color="gray")
